c5d23e100216f9a3e50e1e35068347f9c96ca18f
Bhavya <Bhavya@admins-MacBook-Pro.local>
2018-06-27 10:52:04 +0100

    3 Types of Invaders Created

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -3,6 +3,7 @@ import time
 import random
 from pygame.locals import *
 
+
 #Initialze pygame
 pygame.init()
 
@@ -44,7 +45,7 @@ def WelcomeScreen(): #initial screen before the game begins
 
 
 
-	if mouse_pos[0] < 535 and mouse_pos[0] > 270 and mouse_pos[1] < 480 and mouse_pos[1] > 400:#If mouse hovers over button
+	if mouse_pos[0] < 535 and mouse_pos[0] > 270 and mouse_pos[1] < 480 and mouse_pos[1] > 400:
 		pygame.draw.rect(screen, RED,(270,400,265,80))
 		if mouse_click[0]:
 			game_state = True
@@ -56,12 +57,33 @@ def WelcomeScreen(): #initial screen before the game begins
 	screen.blit(text_welcome,(280,417))
 
 def Game(): #The game has begin... initialize all classes and display
+	global score,highest_score
+
+	background = pygame.image.load('background.png')
+	screen.blit(background,(0,0))
+	text_score = font_comic.render('SCORE:', False, GOLD)
+	screen.blit(text_score,(150,25))
+	text_score = font_comic.render(str(score), False, ORANGE)
+	screen.blit(text_score,(270,25))
+	text_score = font_comic.render('HIGH SCORE:', False, GOLD)
+	screen.blit(text_score,(430,25))
+	text_score = font_comic.render(str(highest_score), False, ORANGE)
+	screen.blit(text_score,(630,25))
+
+
+	#display all objects 
 	pass
 
-def Left(): #Move spaceship towards left
+def Left_press(): #increase velocity
 	pass
 
-def Right(): #move spaceship towards right
+def Right_press(): #increase velocity
+	pass
+
+def Release_arrowkey(): #velocity=0
+	pass
+
+def Fire_Spaceship(): #release a bullet from same x coordinate upwards
 	pass
 
 # Spaceship object and starting coordinates
@@ -72,19 +94,49 @@ ship_x = screen.get_width()/2 - ship.get_width()/2
 def Spaceship(ship,ship_x,ship_y):
 	screen.blit(ship,(ship_x,ship_y))
 
-# Invader Object and Class
-invader = font_comic.render('Invader',False,RED) # This line to be replaced by Invader image
+# Invader Class
+invader1 = font_comic.render('Invader1',False,RED) # This line to be replaced by Invader image
+invader2 = font_comic.render('Invader2',False,GREY) # This line to be replaced by Invader image
+invader3 = font_comic.render('Invader3',False,BLUE) # This line to be replaced by Invader image
 
-class Invader:
-	invader_x = random.randrange(0,screen.get_width())
-	invader_y = random.randrange(0,400)
-	def __init__(self,invader,invader_x,invader_y):
-		self.invader = invader
-		
-	def get_invader(self):
-		screen.blit(self.invader,(self.invader_x,self.invader_y))
+class Invader1:
+	def __init__(self,invader1,invader_x,invader1_y,score):
+		self.invader1 = invader1
+		self.invader_x = invader_x
+		self.invader1_y = invader1_y
+	
+	def Get_Invader(self):
+		screen.blit(self.invader1,(self.invader_x,self.invader1_y))
 
+	def Invader_Attack(self):
+		self.score += 10
+		return self.score
 
+class Invader2:
+	def __init__(self,invader2,invader_x,invader2_y,score):
+		self.invader2 = invader2
+		self.invader_x = invader_x
+		self.invader2_y = invader2_y
+	
+	def Get_Invader(self):
+		screen.blit(self.invader2,(self.invader_x,self.invader2_y))
+
+	def Invader_Attack(self):
+		self.score += 20
+		return self.score
+
+class Invader3:
+	def __init__(self,invader3,invader_x,invader3_y,score):
+		self.invader3 = invader3
+		self.invader_x = invader_x
+		self.invader3_y = invader3_y
+	
+	def Get_Invader(self):
+		screen.blit(self.invader3,(self.invader_x,self.invader3_y))
+
+	def Invader_Attack(self):
+		self.score += 30
+		return self.score
 
 
 ##MAIN
@@ -100,17 +152,20 @@ while not quit:
 	for event in pygame.event.get():       
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
-				Left()
+				Left_press()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
-				Right()
+				Right_press()
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				Release_arrowkey()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE:
 				Fire_Spaceship()
 		if event.type == pygame.QUIT:
 			quit = True
     
-	clock.tick(10) #Sets FPS of the game
+	clock.tick(20) #Sets FPS of the game
 	pygame.display.update()
 
 pygame.quit()
